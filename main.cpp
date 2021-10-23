69ac3911cd6f7c400846b0b490f7bd646163c28c
Janthony125 <janthony125@csu.fullerton.edu>
2021-10-23 15:47:19 -0700

    Co-authored-by: Cody Mangham <CodyMang@users.noreply.github.com> Co-authored-by: Sydnie Nguyen <sydnienguyen@users.noreply.github.com>

--- main.cpp
+++ main.cpp
@@ -1,143 +1,99 @@
 #include <iostream>
-#include <regex>
+#include <fstream>
 #include <string>
-
+#include <cctype>
+#include <vector>
+#include <regex>
 using namespace std;
-const string KEYWORDS = "alignas" 
-                    "|alignof"
-                    "|and"
-                    "|and_eq"
-                    "|asm"
-                    "|atomic_cancel"
-                    "|atomic_commit"
-                    "|atomic_noexcept"
-                    "|auto"
-                    "|bitand"
-                    "|bitor"
-                    "|bool"
-                    "|break"
-                    "|case"
-                    "|catch"
-                    "|char"
-                    "|char8_t"
-                    "|char16_t"
-                    "|char32_t"
-                    "|class"
-                    "|compl"
-                    "|concept"
-                    "|const"
-                    "|consteval"
-                    "|constexpr"
-                    "|constinit"
-                    "|const_cast"
-                    "|continue"
-                    "|co_await"
-                    "|co_return"
-                    "|co_yield"
-                    "|decltype"
-                    "|default"
-                    "|delete"
-                    "|do"
-                    "|double"
-                    "|dynamic_cast"
-                    "|else"
-                    "|enum"
-                    "|explicit"
-                    "|export"
-                    "|extern"
-                    "|false"
-                    "|float"
-                    "|for"
-                    "|friend"
-                    "|goto"
-                    "|if"
-                    "|inline"
-                    "|int"
-                    "|long"
-                    "|mutable"
-                    "|namespace"
-                    "|new"
-                    "|noexcept"
-                    "|not"
-                    "|not_eq"
-                    "|nullptr"
-                    "|operator"
-                    "|or"
-                    "|or_eq"
-                    "|private"
-                    "|protected"
-                    "|public"
-                    "|reflexpr"
-                    "|register"
-                    "|reinterpret_cast"
-                    "|requires"
-                    "|return"
-                    "|short"
-                    "|signed"
-                    "|sizeof"
-                    "|static"
-                    "|static_assert"
-                    "|static_cast"
-                    "|struct"
-                    "|switch"
-                    "|synchronized"
-                    "|template"
-                    "|this"
-                    "|thread_local"
-                    "|throw"
-                    "|true"
-                    "|try"
-                    "|typedef"
-                    "|typeid"
-                    "|typename"
-                    "|union"
-                    "|unsigned"
-                    "|using"
-                    "|virtual"
-                    "|void"
-                    "|volatile"
-                    "|wchar_t"
-                    "|while"
-                    "|xor"
-                    "|xor_eq";
-
-bool isPunctuator(string ch){
-    if(ch.length() != 1 || ch == "@") return false;
-    regex punctuator("[[:blank:][:punct:]]");
-    return regex_match(ch,punctuator);
-}
 
 bool isKeyword(const string& str){
-    regex keywords(KEYWORDS);
-    if(regex_match(str,keywords)){
-        cout << str << " is a keyword" <<endl;
-        return true;
-    } 
-    cout << str << " is not a keyword" <<endl;
+    string keywords[32] = {"auto","break","case","char","const","continue","default","do","double","else","enum","extern","float","for","goto","if","int","long","register","return","short","signed","sizeof","static","struct","switch","typedef","union","unsigned","void","volatile","while"};
+    
+    for(int i = 0; i < 32; ++i){
+        if(keywords[i] == str){
+            cout << str << " is a keyword\n";
+            return true;
+        }
+    }
     return false;
 }
 
-void parse(const string& file){
-    int left = 0, right = 0;
-    int len = file.length();
-    // while(right <= len && left <= right){
-
-    // }
-
-}
 
 
-
-int main() {
-
-    cout << "Testing keywords" << endl;
-    string tests = " +-*/\\,;><=()[]{}&|^?@abcAR";
-    
-    for(char c : tests){
-        string test(1,c);
-        cout << test 
-        << (isPunctuator(test) ? " is a Punctuator":" is not a Punctuator") << endl;
+bool isOperator(const char& str){
+    char operators[] = "+-*/%=><";
+    for(int i = 0; i < 8; ++i) {
+        if(str == operators[i]){
+            return true;
+        }
     }
-    return 0;
-
+    return false;
 }
+
+bool isSeparator(const char& ch) {
+    char separator[] = "{}()[]";
+    for(int i = 0; i < 6; i++){
+        if(separator[i] == ch){
+            return true;
+        }
+    }
+    return false;
+}
+
+bool isAlnum(const string& str){
+    for(char c: str){
+        if(!isalnum(c)) return false;
+    }
+    return true;
+}
+
+void identify(const string& currentToken) {
+    if(!isKeyword(currentToken) && currentToken != ""){
+        if(isalpha(currentToken[0]) && isAlnum(currentToken)){
+            cout << currentToken << " is an identifier\n";
+        } else if(regex_match(currentToken,regex("[[:digit:]]+"))){
+            cout << currentToken << " is an INT value\n";
+        } else if(regex_match(currentToken,regex("[+-]?([0-9]*[.])?[0-9]+"))){
+            cout << currentToken << " is a FlOAT value\n";
+        }
+    }
+}
+int main() {
+    char line[20];
+    std::ifstream programFile;
+    programFile.open("test.txt");
+    if (programFile.fail()) {
+        std::cerr << "Unable to open file for reading." << std::endl;
+        exit(1);
+    }
+    char nextToken;
+    string currentToken = "";
+    while (!programFile.eof()) {
+        //std::cout << nextToken << std::endl;
+        nextToken = programFile.get();
+        // cout << "Current token: " << nextToken << "\n";
+        if(isalnum(nextToken) || nextToken == '.'){
+            currentToken += nextToken;
+        }
+        else if(isSeparator(nextToken)){
+            identify(currentToken);
+            cout << nextToken << " is a separator\n";
+            currentToken = "";
+        }
+        else if(isOperator(nextToken)){
+            identify(currentToken);
+            cout << nextToken << " is a operator\n";
+            currentToken = "";
+        }
+        else if(nextToken == ' ' || nextToken == '\n' || nextToken == ';'){
+            identify(currentToken);
+            currentToken = "";
+        }
+        
+    }
+    
+    identify(currentToken);
+    programFile.close();
+    return 0;
+}
+
